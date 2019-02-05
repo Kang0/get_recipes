7574f21eb37826e6bf6f26b1d3d016714c52c5dc
Kevin Kang <kevinkangg@gmail.com>
2019-02-05 21:24:02 +0000

    updating relationship methods

--- lib/get_recipes/cli.rb
+++ lib/get_recipes/cli.rb
@@ -2,9 +2,7 @@ class GetRecipes::CLI
   
   def call
     list_cuisines
-    cuisine_recipe
-    individual_recipe
-    exit_program
+    cuisine_recipes
   end
   
   def list_cuisines
@@ -17,33 +15,57 @@ class GetRecipes::CLI
     puts "5. French"
   end
   
-  def cuisine_recipe
+  def cuisine_recipes
     #Once the appropriate cuisine is selected, scrape the cuisine recipe and provide all the recipes for that specific cuisine
     #input a .between?(number of recipes) and use the selected input to provide the cuisine recipe list
-    user_input = gets.strip
-    if input == "exit"
-      exit_program
-    elsif user_input.to_i.between?(1-5)
-      individual_recipe(user_input.to_i)
-      
-      # if user_input == "1"
-      #   puts "American Recipes"
-      # elsif user_input == "2"
-      #   puts "British Recipes"
-      # elsif user_input == "3"
-      #   puts "Caribbean Recipes"
-      # elsif user_input == "4"
-      #   puts "Chinese Recipes"
-      # elsif user_input == "5"
-      #   puts "French Recipes"
+    user_input = nil
+    while user_input != "exit"
+      puts "Which 'American' recipe would you like to make?"
+      user_input = gets.strip.downcase
+      puts "1. Buffalo Chicken"
+      puts "2. Texas BBQ Medley"
+      puts "3. S'mores Dip"
+      puts "4. Fully Loaded Cajun Chicken Burgers"
+      puts "5. Tomato, Avocado & Corn Salad With Migas & Buttermilk Dressing"
+      case user_input
+      when user_input.to_i.between?(1,5)
+        individual_recipe(user_input)
+      when "back"
+        list_cuisines
+      when "exit"
+        exit_program
+      else
+        "Please type the recipe number, type back to go back to the cuisine list, or type exit."
       end
-    end
   end
   
   def individual_recipe(user_input)
     #scrape the selected recipe webpage and provide the recipe information to the user
-    
-    
+    ind_recipe_input = nil
+    while ind_recipe_input != "exit"
+      ind_recipe_input = gets.strip
+      
+      puts "Serving Size - 4"
+      puts "Nutritional Information: "
+      puts "kcal - 500"
+      puts "Fat - 5g"
+      puts "Ingredients:"
+      puts "Buffalo Sauce"
+      puts "Chicken Thighs"
+      puts "Method:"
+      puts "Place in Oven"
+      puts "Type list to return to the Cuisine List, type back to go back to 'American' recipes, or type exit."
+      
+      case ind_recipe_input
+      when "list"
+        list_cuisines
+      when "back"
+        cuisine_recipes
+      when "exit"
+        exit_program
+      else
+        "Please type list to return to the Cuisine List, type back to go back to 'American' recipes, or type exit."
+      end
   end
   
   def exit_program
