cc6e2a2d67f7047703b7aa1d7a511c01ae201bc4
Kevin Kang <kevinkangg@gmail.com>
2019-02-07 15:53:09 +0000

    finished nutrition hash, ingredient and method array

--- lib/get_recipes/cli.rb
+++ lib/get_recipes/cli.rb
@@ -67,19 +67,20 @@ class GetRecipes::CLI
     #scrape the selected recipe webpage and provide the recipe information to the user
     #need to iterate over the user inputted recipe number and provide the information below
     
-    GetRecipes::Recipe.ingredients.each.with_index(1) do |attributes, index|
-      puts "Serving Size - #{attributes.serving_size}"
-      puts "Nutrional Information:"
-      puts "kcal - #{} / fat - #{} / saturates - #{} / carbs - #{} / sugars - #{} / fibre - #{} / protein - #{} / salt - #{}"
-      puts "Ingredients:"
-      puts "#{attributes.ingredients}"
-      puts "Methods:"
-      puts "#{attributes.methods}"
-    end
+    # GetRecipes::Recipe.ingredients.each.with_index(1) do |attributes, index|
+    #   puts "Serving Size - #{attributes.serving_size}"
+    #   puts "Nutrional Information:"
+    #   puts "kcal - #{} / fat - #{} / saturates - #{} / carbs - #{} / sugars - #{} / fibre - #{} / protein - #{} / salt - #{}"
+    #   puts "Ingredients:"
+    #   puts "#{attributes.ingredients}"
+    #   puts "Methods:"
+    #   puts "#{attributes.methods}"
+    # end
 
     puts "Type list to return to the Cuisine List or type exit."
     
     ind_recipe_input = gets.strip.downcase
+    
     if ind_recipe_input == "exit"
       exit_program
     elsif ind_recipe_input == "list"
--- lib/get_recipes/recipe.rb
+++ lib/get_recipes/recipe.rb
@@ -2,24 +2,22 @@ class GetRecipes::Recipe
   
   attr_accessor :serving_size, :nutritional, :ingredients, :methods
   
-  def self.ingredients
-    recipe = self.new
-    recipe.serving_size = 4
-    recipe.nutritional = {"kcal" => "4", "fat" => "5g", "saturates" => "6g", "carbs" => "10g", "sugars" => "3g", "fibre" => "2g", "protein" => "1g", "salt" => "4g"}
-    recipe.ingredients = ["milk", "eggs", "flour", "chicken thighs", "salt"]
-    recipe.methods = ["Tear the bread into pieces and toss in a bowl with the milk", "Meanwhile, hold a corn cob standing up in a shallow roasting tin. Using a sharp knife,", "To make the dressing, put the mayonnaise, mustard, garlic and chives in a bowl,"]
-  end
+  @@all = []
   
   def initialize(serving_size = nil, nutritional = {}, ingredients = [], methods = [])
     @serving_size = serving_size
     @nutritional = nutritional
     @ingredients = ingredients
     @methods = methods
+    @@all << self
   end
   
   def self.create_recipe_attribute(recipe)
+    
     nutrition_hash = {}
     ingredients_array = []
+    methods_array = []
+    
     recipe.css(".nutrition li").each do |nutrition|
       nutrition_hash[nutrition.css(".nutrition__label").text] = nutrition.css(".nutrition__value").text
     end
@@ -27,25 +25,25 @@ class GetRecipes::Recipe
     recipe.css(".ingredients-list__group li").each do |ingredient|
       ingredients_array << ingredient["content"]
     end
+    
+    recipe.css(".method__item").each do |method|
+      methods_array << method.text
+    end
 
-    binding.pry
-    
-    # self.new(
-    #   recipe.css(".recipe-details__item--servings").text.strip,
-    #   nutrition_hash,
-      
-    #   )
-    
-    #serving_size - doc.css(".recipe-details__item--servings").text.strip
-    #nutrition - doc.css(".nutrition")
-      #will have to iterate over this and assign the key and value into a hash
-    #ingredients - doc.css(".ingredients-list__group")[1].elements[0].content
-      #will have to iterate over ".ingredients-list__group" starting at index 1 and then iterating over this information and collecting .elements and its content
-      #then push this into an array of ingredients
-    #methods - doc.css(".method__item p")
-      #will have to iterate over this and push each text box into an array
+    self.new(
+      recipe.css(".recipe-details__item--servings").text.strip,
+      nutrition_hash,
+      ingredients_array,
+      methods_array
+      )
     
   end
+  
+  def self.all
+    @@all
+  end
+  
+  #possibly create another method to handle creating the arrays and hashes 
   
   
 end
